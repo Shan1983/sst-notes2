397df635bb7bd942cbf785666df2da2c1ba7aaa6
Shannan Galbraith <s.galbraith08@gmail.com>
2022-10-23 03:39:28 +1100

    added delete lambda

---
+++ services/functions/delete.js
@@ -0,0 +1,16 @@
+import handler from "../utils/handler";
+import dynamodb from "../utils/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      userId: "123",
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  await dynamodb.delete(params);
+
+  return { status: true };
+});
